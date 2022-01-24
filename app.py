34559e9a3deabdb19e7da603905df437393ee7c7
jebincvarghese <43873109+jebincvarghese@users.noreply.github.com>
2022-01-24 23:31:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<h1><center>Flask Application version 1 </h1></center>"
+  return "<h1><center>jomo muthumaniiii </h1></center>"
 
 
 app.run(host='0.0.0.0', port=5000)
